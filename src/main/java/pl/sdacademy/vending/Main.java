a7fb0077a4aa2adcb3ae2ee2cba26bc0a3b42f95
Pawel Bialas <p.cz.bialas@gmail.com>
2018-11-17 10:50:19 +0100

    menu product buying added

--- src/main/java/pl/sdacademy/vending/Main.java
+++ src/main/java/pl/sdacademy/vending/Main.java
@@ -1,9 +1,11 @@
 package pl.sdacademy.vending;
 
 import pl.sdacademy.vending.controller.CustomerOperationsController;
+import pl.sdacademy.vending.model.Product;
 import pl.sdacademy.vending.model.VendingMachine;
 import pl.sdacademy.vending.util.Configuration;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
@@ -17,17 +19,33 @@ public class Main {
         while (true) {
             customerOperationsController.printMachine();
             printMenu();
-            UserMenuSelection userSelection = getUserSelection();
+            try {
 
-            switch (userSelection) {
-                case BUY_PRODUCT:
-                    //TODO
-                    break;
-                case EXIT:
-                    System.out.println("Bye!");
-                    return;
-                default:
-                    System.out.println("Ibvalid selection");
+                UserMenuSelection userSelection = getUserSelection();
+
+                switch (userSelection) {
+                    case BUY_PRODUCT:
+                        //1. pobrać symbol tacki
+                        //2. wywołać odpowiednią metodę z controlera (Optional buy product for symbol (String traySymbol)
+                        //.3 Jeżeli udało się kupić produkt to wypisujemy, że udało się kupić oraz nazwę
+                        //4. Jeżeli nit ro wyświetlamy brak produktu
+                        System.out.println("Please select tray symbol");
+                        String userProductSelection = new Scanner(System.in).nextLine();
+                        Optional<Product> product = customerOperationsController.buyProductForSymbol(userProductSelection);
+                        if (product.isPresent()) {
+                            System.out.println("Sales completed: " + product.get().getName());
+                        } else
+                            System.out.println("Out of product");
+
+                        break;
+                    case EXIT:
+                        System.out.println("Bye!");
+                        return;
+                    default:
+                        System.out.println("Ibvalid selection");
+                }
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
             }
         }
     }
@@ -41,14 +59,16 @@ public class Main {
     }
 
     private UserMenuSelection getUserSelection() {
+        System.out.print("> Your selection: ");
         String userSelection = new Scanner(System.in).nextLine();
         try {
             Integer menuNumber = Integer.valueOf(userSelection);
+            return UserMenuSelection.selectionForOptionNumber(menuNumber);
 
         } catch (NumberFormatException e) {
-            return null;
+            throw new IllegalArgumentException("Invalid selection fomat");
         }
-        return null;
+
     }
 
     public static void main(String[] args) {
