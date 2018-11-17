25c853f633a5093d290222e972d69630fa4e599c
Pawel Bialas <p.cz.bialas@gmail.com>
2018-11-17 10:02:11 +0100

    menu by stream

--- src/main/java/pl/sdacademy/vending/UserMenuSelection.java
+++ src/main/java/pl/sdacademy/vending/UserMenuSelection.java
@@ -1,9 +1,11 @@
 package pl.sdacademy.vending;
 
+import java.util.Arrays;
+
 public enum UserMenuSelection {
 
-    BUY_PRODUCT (1, "Buy product"),
-    EXIT (9, "Exit");
+    BUY_PRODUCT(1, "Buy product"),
+    EXIT(9, "Exit");
 
     private final Integer optopnNumber;
     private final String optionText;
@@ -14,14 +16,20 @@ public enum UserMenuSelection {
         this.optionText = optionText;
     }
 
-    public static UserMenuSelection selectionForOptionNumber (Integer requestedOptionNumber) {
-        UserMenuSelection[] values = UserMenuSelection.values();
-        for (UserMenuSelection menuSelection : values) {
-            if (menuSelection.getOptionNumber().equals(requestedOptionNumber)) {
-                return menuSelection;
-            }
-    }
-    throw new IllegalArgumentException("Unknown option number: " + requestedOptionNumber); 
+    public static UserMenuSelection selectionForOptionNumber(Integer requestedOptionNumber) {
+//        UserMenuSelection[] values = UserMenuSelection.values();
+//        for (UserMenuSelection menuSelection : values) {
+//            if (menuSelection.getOptionNumber().equals(requestedOptionNumber)) {
+//                return menuSelection;
+//            }
+//    }
+//    throw new IllegalArgumentException("Unknown option number: " + requestedOptionNumber);
+//      Wersja z pętlą.
+
+        return Arrays.stream(values())
+                .filter(enumValue -> enumValue.getOptionNumber().equals(requestedOptionNumber))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Unknown option number: " + requestedOptionNumber));
     }
 
     public Integer getOptionNumber() {
